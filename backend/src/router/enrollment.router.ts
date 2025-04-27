82dacd4ef01f7d5b1189622ea47c6f315d694470
sonnie36 <sonniemwati36@gmail.com>
2025-04-27 21:13:12 +0300

    fix enrollments status update

--- backend/src/router/enrollment.router.ts
+++ backend/src/router/enrollment.router.ts
@@ -14,6 +14,7 @@ const enroll_router = Router();
 enroll_router.post("/enroll",authorizeDoctor, enrollClientController);
 enroll_router.get("/client/:clientId", getClientEnrollmentsController);
 enroll_router.get("/program/:id", getProgramEnrollmentsController);
+enroll_router.put("/update/:id", authorizeDoctor, updateEnrollmentStatusController);
 enroll_router.patch("/enrollments/:id", authorizeDoctor,updateEnrollmentStatusController);
 enroll_router.delete("/enrollments/:id",authorizeDoctor, deleteEnrollmentController);
 enroll_router.get("/all", authorizeDoctor, getAllEnrollmentsController);
